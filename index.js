62ad46cf1926c279eae6350a69ae3ec67c177497
adrian7villa <adria.david.villa7@gmail.com>
2021-08-17 15:19:39 -0600

    segundo commit con modificaciones

---
+++ index.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded',function(){
+    const txtTitulo=document.getElementById('title');
+    const txtDescripcion=document.getElementById('description');
+    const btnAgregar=document.getElementById('add');
+})
