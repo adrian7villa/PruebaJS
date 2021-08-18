95a54d1a8dc93abeb7eebdf648cca6624f3333f4
adrian7villa <adria.david.villa7@gmail.com>
2021-08-18 15:57:51 -0600

    Se imprime el valor de titulo

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ document.addEventListener('DOMContentLoaded',function(){
     const txtTitulo=document.getElementById('title');
     const txtDescripcion=document.getElementById('description');
     const btnAgregar=document.getElementById('add');
+
+
+
+    btnAgregar.onclick=function(){
+        console.log('Titulo: ',txtTitulo.value);
+    }
 })
