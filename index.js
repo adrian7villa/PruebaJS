df8aa00e4db18065645dedba818ffad30559acc4
adrian7villa <adria.david.villa7@gmail.com>
2021-08-18 15:52:49 -0600

    Variable de tabla

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded',function(){
     const txtTitulo=document.getElementById('title');
     const txtDescripcion=document.getElementById('description');
+    const TDatos=document.getElementById('table');
     const btnAgregar=document.getElementById('add');
+
+
 })
