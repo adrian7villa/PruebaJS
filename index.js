95ae8a6580945ebe45bc5fa523c02ba12938ceae
adrian7villa <adria.david.villa7@gmail.com>
2021-08-18 16:40:07 -0600

    Se agrego el txt descripcion para mostrar en la consola

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ document.addEventListener('DOMContentLoaded',function(){
     const txtTitulo=document.getElementById('title');
     const txtDescripcion=document.getElementById('description');
     const btnAgregar=document.getElementById('add');
+
+    btnAgregar.onclick=function(){
+        console.log('Titulo: ',txtTitulo.value);
+        console.log('Descripcion: ', txtDescripcion.value);
+    }
 })
