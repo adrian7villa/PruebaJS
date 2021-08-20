5d74f68c500afd328ef0716f71ffe90711b67043
adrian7villa <adria.david.villa7@gmail.com>
2021-08-19 19:52:57 -0600

    Accion de boton al poner vacios

--- index.js
+++ index.js
@@ -3,13 +3,31 @@ document.addEventListener('DOMContentLoaded',function(){
     const txtDescripcion=document.getElementById('description');
     const TDatos=document.getElementById('table');
     const btnAgregar=document.getElementById('add');
+    const LAlerta=document.getElementById('alert');
 
-    btnAgregar.onclick=function(){
+
+    /*btnAgregar.onclick=function(){
         console.log('Titulo: ',txtTitulo.value);
         console.log('Descripcion: ', txtDescripcion.value);
+        
         //esta es la mas reciente
         //se agrega nueva linea para prueba1
         //prueb2
         //prueba de remoto con rama prueba 2 clonada
+    }*/
+
+    function addTodo(){
+        if(txtTitulo.value==='' || txtDescripcion.value===''){
+            //console.error('El titulo y la descripcion es requerido');
+            LAlerta.classList.remove('d-none');
+            LAlerta.innerText='El titulo y la descripcion son requerido';
+            return;
+        }
+        
+        LAlerta.classList.add('d-none');
     }
-})
+
+    btnAgregar.onclick=addTodo;
+
+
+});
