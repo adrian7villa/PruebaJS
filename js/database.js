953a3b70e895cf0005878203e75b8e30f54329c7
adrian7villa <adria.david.villa7@gmail.com>
2021-09-07 17:49:48 -0600

    se quitaron los parametros de dtabase por error

--- js/database.js
+++ js/database.js
@@ -1,8 +1,8 @@
 const mongoose=require('mongoose')
 
-mongoose.connect('mongodb://mongo/midatabase',{
-    userNewUrlParser: true,
-    userUnifieldTopology: true
-})
+mongoose.connect('mongodb://mongo/midatabase')//,{
+    //userNewUrlParser: true,
+    //userUnifieldTopology: true
+//})
     .then(db=>console.log('Db is conected to',db.connection.host))
     .catch(err=>console.error(err));
