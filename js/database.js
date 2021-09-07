068c5ab1b57ae64524342c32a8829cfd12cb9de6
adrian7villa <adria.david.villa7@gmail.com>
2021-09-07 17:46:52 -0600

    cambio en database js parametros para no tener erro

--- js/database.js
+++ js/database.js
@@ -1,5 +1,8 @@
 const mongoose=require('mongoose')
 
-mongoose.connect('mongodb://mongo/midatabase')
+mongoose.connect('mongodb://mongo/midatabase',{
+    userNewUrlParser: true,
+    userUnifieldTopology: true
+})
     .then(db=>console.log('Db is conected to',db.connection.host))
     .catch(err=>console.error(err));
